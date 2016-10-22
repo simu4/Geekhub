14d8c88985c44a19b194e8ac3cccdf6b3cc17a79
Kirill Netavskiy <kirillnetavskiy@macbook-pro-kirill.local>
2016-10-22 23:29:25 +0300

    Initial Commit

---
+++ Lesson1Task3_1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lesson1Task3_1
+//
+//  Created by Kirill Netavskiy on 22.10.16.
+//  Copyright © 2016 Kirill Netavskiy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
